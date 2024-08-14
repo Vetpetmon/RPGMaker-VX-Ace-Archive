4d3e8f0a688716cdbcd0142d8ebe904ef516fcaa
Modrome <vetpetmon@hotmail.com>
2024-08-14 14:40:28 -0400

    Create MP Peril.rb

---
+++ Scripts/MP Peril.rb
@@ -0,0 +1,30 @@
+=begin
+  Peril Status for VX Ace (MP Edition)
+  version 0
+  
+  By: Modrome
+  
+    An MP version of the Peril script,
+    this applies an effect similar to 
+    Peril, but instead of taking HP into
+    account, this uses the MP value.
+
+    This script allows you to create
+    characters who use their MP as their 
+    "health" pool, instead of HP.
+
+    To use this, the actor needs to 
+    be vulnerable to the MP Peril status ID.
+    To prevent other actors from getting 
+    this status, their vulnerability needs
+    to be set to 0% or "immune" for the status
+    ID.
+
+    NOTE: This script is not yet complete.
+    I made this file ahead of time as a 
+    self-reminder. Yell at me or something if
+    I forget to write this before Friday lol
+
+            
+  COPYRIGHT: Apache License 2.0 
+=end
