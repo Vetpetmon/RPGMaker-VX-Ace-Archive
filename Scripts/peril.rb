9ebf7a8818d01657ea05e996f2991fa3b7441e93
Modrome <vetpetmon@hotmail.com>
2024-08-21 16:18:31 -0400

    HP Peril ver 2
    Now with 10% less hardcoding values!

--- Scripts/peril.rb
+++ Scripts/peril.rb
@@ -1,6 +1,6 @@
 =begin
-  Peril Status for VX Ace
-  version 1
+  HP Peril Status for VX Ace
+  version 2
   
   By: Modrome
   
@@ -16,6 +16,11 @@
 =end
 module Peril
   PERILSTATEID = 29 # Change this to the ID of the status effect.
+  
+  # VER 2
+  # The hp % the actor must fall below in order
+  #	for Peril to be applied.
+  PERILTHRESHHOLD = 25
 end
 
 
@@ -25,7 +30,7 @@ class Game_Actor
   def refresh
     og_refresh
     return unless Peril::PERILSTATEID
-    if hp_rate < (0.25)
+    if hp_rate < (Peril::PERILTHRESHHOLD/100)
       add_state(Peril::PERILSTATEID) unless state?(Peril::PERILSTATEID)
     else
       erase_state(Peril::PERILSTATEID) if state?(Peril::PERILSTATEID)
