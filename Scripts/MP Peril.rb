7403ee58ea1f4cd9498b5930cda92802701aaf24
Modrome <vetpetmon@hotmail.com>
2024-08-22 23:31:13 -0400

    Update MP Peril.rb

--- Scripts/MP Peril.rb
+++ Scripts/MP Peril.rb
@@ -9,22 +9,60 @@
     Peril, but instead of taking HP into
     account, this uses the MP value.
 
+    HP Peril is still included.
+
     This script allows you to create
     characters who use their MP as their 
     "health" pool, instead of HP.
 
-    To use this, the actor needs to 
-    be vulnerable to the MP Peril status ID.
-    To prevent other actors from getting 
-    this status, their vulnerability needs
-    to be set to 0% or "immune" for the status
-    ID.
+    Class ID is used to determine which 
+    Actor(s) will have this feature.
 
-    NOTE: This script is not yet complete.
-    I made this file ahead of time as a 
-    self-reminder. Yell at me or something if
-    I forget to write this before Friday lol
+    Precaution: One or the other.
+    Using both scripts will incur a 
+    Stack Level Too Deep error.
 
             
   COPYRIGHT: Apache License 2.0 
 =end
+
+# CONFIGURATION START
+module Peril
+  PERILSTATEID = 29 # Change this to the ID of the status effect.
+end
+
+module MPPeril
+  MPPERILSTATEID = 80 # Change this to the ID of the status effect.
+  MPPERILTHRESHHOLD = 50 # default: 25%
+  
+  MPPERILCLASSES=[8,] # Add numbers (with comma separators) 
+                      # to include more classes
+end
+
+# CONFIGURATION END
+
+
+class Game_Actor
+  
+  alias og_refresh refresh
+  def refresh
+    og_refresh
+
+    # MP PERIL
+    if MPPeril::MPPERILCLASSES.include?(class_id)
+        if mp_rate < (0.25)
+        add_state(MPPeril::MPPERILSTATEID) unless state?(MPPeril::MPPERILSTATEID)
+      else
+        erase_state(MPPeril::MPPERILSTATEID) if state?(MPPeril::MPPERILSTATEID)
+      end
+    end
+
+    # HP PERIL
+    if hp_rate < (0.25)
+      add_state(Peril::PERILSTATEID) unless state?(Peril::PERILSTATEID)
+    else
+      erase_state(Peril::PERILSTATEID) if state?(Peril::PERILSTATEID)
+    end
+  end
+  
+end
